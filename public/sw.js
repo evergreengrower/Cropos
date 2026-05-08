6a71857541e79e82240e9239c03615f9205ffb27
Berna <berna.mnac02@gmail.com>
2026-05-07 22:43:01 -0300

    fix: recuperar partidas y movimientos de Medplant

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,18 @@
+const VERSION = '2026-05-08-v18'
+
 self.addEventListener('install', () => self.skipWaiting())
+
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   )
 })
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)))
+
+// Sin caché: siempre red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request, { cache: 'no-store' }).catch(() => caches.match(e.request))
+  )
+})
